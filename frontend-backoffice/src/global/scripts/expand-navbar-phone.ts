7f21f6409f8115238e923f32b9b115ec171e7ff4
Gabriele Speciale <gabspec@gmail.com>
2024-12-24 15:08:35 +0100

    feat: complete the sidebar/navbar of the backoffice (minimal, expanded) on phone devices, implementation of the dashboard home, modified files global structure

---
+++ frontend-backoffice/src/global/scripts/expand-navbar-phone.ts
@@ -0,0 +1,14 @@
+const sideBar = document.querySelector("nav#sidebar")
+const toggleBtn = document.querySelector("div.toggleBtn ion-icon");
+let isExpanded: boolean = false;
+
+toggleBtn?.addEventListener("click", () => {
+    if (isExpanded) {
+        sideBar?.classList.remove("expanded");
+        isExpanded = false;
+    } else {
+        sideBar?.classList.add("expanded");
+        isExpanded = true;
+    }
+
+});
--- frontend-backoffice/src/pages/dashboard/main.ts
+++ frontend-backoffice/src/pages/dashboard/main.ts
@@ -1,8 +1,10 @@
+// styles imports
 import "../../global/styles/reset.css";
 import "../../global/styles/global.css";
 import "../../global/styles/navbar-phone.css";
 import "../../global/styles/navbar-tablet.desktop.css";
-
-
 import "./styles/style.css";
 
+
+// scripts imports
+import "../../global/scripts/expand-navbar-phone.ts";
