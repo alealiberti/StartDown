47ff4f9b0604f3ba79153cc9de0306dad4675453
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2024-12-30 16:38:17 +0100

    init: Creation of the api server enviroment (#90)

---
+++ backend/cascina-caccia/src/test/java/com/startdown/cascinacaccia/CascinaCacciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startdown.cascinacaccia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CascinaCacciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
