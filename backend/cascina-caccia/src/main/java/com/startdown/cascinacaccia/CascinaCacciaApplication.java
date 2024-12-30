a967a58d3cbfc15695b67d53666e53dc17b81607
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2024-12-30 16:37:47 +0100

    init: Creation of the api server enviroment

---
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/CascinaCacciaApplication.java
@@ -0,0 +1,13 @@
+package com.startdown.cascinacaccia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CascinaCacciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CascinaCacciaApplication.class, args);
+    }
+
+}
