2d74a8cb2f38542f9c06a9b04fdd9187e32c12c2
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2025-01-02 17:46:56 +0100

    feat: added comments in the DAOs

--- backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/repos/UserDAO.java
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/repos/UserDAO.java
@@ -3,5 +3,9 @@ package com.startdown.cascinacaccia.repos;
 import com.startdown.cascinacaccia.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+The UserDAO interface provides methods for accessing and manipulating user entities in the database.
+It extends JpaRepository, allowing for CRUD operations and more.
+*/
 public interface UserDAO extends JpaRepository<User, Integer> {
 }
