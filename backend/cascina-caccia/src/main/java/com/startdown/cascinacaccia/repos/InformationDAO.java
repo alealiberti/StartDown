24ac6c84673f5b19097300ded9909c73c7d47466
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2025-01-02 17:55:39 +0100

    Feat/DAOs (#96)
    * feat: added comments in the DAOs
    
    * feat: implemented UserDAO

--- backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/repos/InformationDAO.java
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/repos/InformationDAO.java
@@ -3,5 +3,9 @@ package com.startdown.cascinacaccia.repos;
 import com.startdown.cascinacaccia.entities.Information;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+The InformationDAO interface provides methods for accessing and manipulating informations entities in the database.
+It extends JpaRepository, allowing for CRUD operations and more.
+*/
 public interface InformationDAO extends JpaRepository<Information, Integer> {
 }
--- backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/repos/ReservationDAO.java
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/repos/ReservationDAO.java
@@ -3,5 +3,9 @@ package com.startdown.cascinacaccia.repos;
 import com.startdown.cascinacaccia.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+The ReservationDAO interface provides methods for accessing and manipulating reservations entities in the database.
+It extends JpaRepository, allowing for CRUD operations and more.
+*/
 public interface ReservationDAO extends JpaRepository<Reservation, Integer> {
 }
