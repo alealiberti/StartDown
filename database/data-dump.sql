ef1a2453fa46837356fb410b713af104d4438af6
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2025-01-05 19:40:33 +0100

    init: created data dump for db
    added account owner

---
+++ database/data-dump.sql
@@ -0,0 +1,12 @@
+--
+-- Data dump for table `users`
+--
+
+INSERT INTO `users` (
+    `id`, `name`, `surname`, `email`, `password`, `role`
+) 
+VALUES
+-- password: Password123*
+(1, 'Owner', 'Owner', 'startdownowner@gmail.com', '$2a$10$hjirmg.QSR7VKZ/wWIxP/.MnG6bPJPqvGYMI90BYJV9v0JpzlDS0O', 'OWNER'),
+-- (2, 'Admin', '1', '', '$2a$10$hjirmg.QSR7VKZ/wWIxP/.MnG6bPJPqvGYMI90BYJV9v0JpzlDS0O', 'ADMIN'),
+-- (3, 'Owner', '2', '', '$2a$10$hjirmg.QSR7VKZ/wWIxP/.MnG6bPJPqvGYMI90BYJV9v0JpzlDS0O', 'ADMIN');
