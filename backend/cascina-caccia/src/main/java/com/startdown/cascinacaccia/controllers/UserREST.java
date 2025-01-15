6fa4beadf065b375bcb0787e52abac4f08a8e3c3
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2025-01-15 15:24:05 +0100

    feat: implemented CORS in the API server

--- backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/controllers/UserREST.java
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/controllers/UserREST.java
@@ -13,18 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.startdown.cascinacaccia.configuration.PasswordChangeRequest;
 import com.startdown.cascinacaccia.entities.User;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/cascina-caccia/users")
 // The UserREST Controller handles the endpoints and the logic for the User-related API requests
