88b73cace25460d0fec2842fd5f11cb2ea0c8a02
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2025-01-15 21:40:20 +0100

    feat: created ReservationDTO class

---
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/entities/ReservationDTO.java
@@ -0,0 +1,51 @@
+package com.startdown.cascinacaccia.entities;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+/* The ReservationDTO class represents the entity "Reservation"
+    with some adjustments to give to the frontend the correct type of data */
+public class ReservationDTO {
+    private Integer id;
+
+    private String name;
+
+    private String surname;
+
+    private String phone;
+
+    private String email;
+
+    private LocalDate dateSend;
+
+    private LocalDate dateStart;
+
+    private LocalDate dateFinish;
+
+    private LocalTime hourStart;
+
+    private LocalTime hourFinish;
+
+    private String status;
+
+    private String typeGroup;
+
+    private Integer visitors;
+
+    private Integer companions;
+
+    private String additionalInfo;
+
+    private boolean mobilityProblems;
+
+}
