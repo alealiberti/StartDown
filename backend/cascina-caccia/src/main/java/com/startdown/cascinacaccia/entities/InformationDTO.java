3d42756eb89f2e69053c8c7fe5205226a346ba91
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2025-01-15 21:36:46 +0100

    feat: implemented dateFormatter in Information API
    Implemented by creating a InformationDTO object to handle the different type of data of the DateSend Date

---
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/entities/InformationDTO.java
@@ -0,0 +1,30 @@
+package com.startdown.cascinacaccia.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+/* The InformationDTO class represents the entity "Information
+    with some adjustments to give to the frontend the correct type of data */
+public class InformationDTO {
+    private Integer id;
+
+    private String name;
+
+    private String surname;
+
+    private String phone;
+
+    private String email;
+
+    private String dateSend;
+
+    private String text;
+
+    private String status;
+}
--- backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/services/InformationService.java
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/services/InformationService.java
@@ -1,10 +1,13 @@
 package com.startdown.cascinacaccia.services;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.startdown.cascinacaccia.entities.Information;
+import com.startdown.cascinacaccia.entities.InformationDTO;
 import com.startdown.cascinacaccia.exceptions.InformationNotFoundException;
 import com.startdown.cascinacaccia.repos.InformationDAO;
 import jakarta.transaction.Transactional;
@@ -25,13 +28,28 @@ public class InformationService {
     @Autowired
     private EmailService emailservice;
 
+    @Autowired
+    private DateConverterService dateConverterService;
+
     /**
      * Retrieves a list of all informations in the system.
      *
-     * @return a List of Informations objects
+     * @return a List of InformationsDTO objects
      */
-    public List<Information> getAllInformations() {
-        return dao.findAll();
+    public List<InformationDTO> getAllInformations() {
+        List<Information> informations = dao.findAll();
+        return informations.stream()
+                .map(information -> new InformationDTO(
+                        information.getId(),
+                        information.getName(),
+                        information.getSurname(),
+                        information.getPhone(),
+                        information.getEmail(),
+                        dateConverterService.formatDateToFrontend(information.getDateSend()),
+                        information.getText(),
+                        information.getStatus()
+                        ))
+                .collect(Collectors.toList());
     }
 
     /**
@@ -40,9 +58,23 @@ public class InformationService {
      * @param id the ID of the information to be retrieved
      * @return an Optional containing the Information object if found, or an empty Optional if not found
      */
-    public Optional<Information> getInformationById(Integer id) {
-        return Optional.ofNullable(dao.findById(id)
-                .orElseThrow(() -> new InformationNotFoundException("Information with ID " + id + " not found.")));
+    public Optional<InformationDTO> getInformationById(Integer id) {
+        Optional<Information> info = dao.findById(id);
+        if (info.isPresent()) {
+            Information information = info.get();
+            return Optional.of(new InformationDTO(
+                    information.getId(),
+                    information.getName(),
+                    information.getSurname(),
+                    information.getPhone(),
+                    information.getEmail(),
+                    dateConverterService.formatDateToFrontend(information.getDateSend()),
+                    information.getText(),
+                    information.getStatus()
+            ));
+        } else {
+            throw new InformationNotFoundException("Information with ID " + id + " not found.");
+        }
     }
 
     /**
@@ -80,11 +112,11 @@ public class InformationService {
      * Updates the details of an existing information.
      *
      * @param id the ID of the information to be updated
-     * @param informationDetails the Information object containing the updated details
+     * @param informationDetails the InformationDTO object containing the updated details
      * @return an Optional containing the updated Information object if successful, or an empty Optional if the information was not found
      */
     @Transactional
-    public Optional<Information> updateInformation(Integer id, Information informationDetails) {
+    public Optional<Information> updateInformation(Integer id, InformationDTO informationDetails) {
         Information existingInformation = dao.findById(id)
                 .orElseThrow(() -> new InformationNotFoundException("Information with ID " + id + " not found."));
 
@@ -105,7 +137,7 @@ public class InformationService {
             existingInformation.setText(informationDetails.getText());
         }
         if (informationDetails.getDateSend() != null) {
-            existingInformation.setDateSend(informationDetails.getDateSend());
+            existingInformation.setDateSend(dateConverterService.parseDate(informationDetails.getDateSend()));
         }
         if (informationDetails.getPhone() != null && !informationDetails.getPhone().trim().isEmpty()) {
             existingInformation.setPhone(informationDetails.getPhone());
@@ -132,20 +164,43 @@ public class InformationService {
      * Retrieves a list of informations with a specific status
      *
      * @param status the name of the status for which the informations is to be retrieved
-     * @return a List containing the Information objects if found, or an empty List if not found
+     * @return a List containing the InformationDTO objects if found, or an empty List if not found
      */
-    public List<Information> getInformationsByStatus(String status) {
+    public List<InformationDTO> getInformationsByStatus(String status) {
         List<Information> informations = dao.findByStatus(status);
-        return informations;
+        return informations.stream()
+                .map(information -> new InformationDTO(
+                        information.getId(),
+                        information.getName(),
+                        information.getSurname(),
+                        information.getPhone(),
+                        information.getEmail(),
+                        dateConverterService.formatDateToFrontend(information.getDateSend()),
+                        information.getText(),
+                        information.getStatus()
+                ))
+                .collect(Collectors.toList());
     }
 
     /**
      * Retrieves a list of all informations sorted in descending order by date send
      *
-     * @return a List containing the Information objects if found, or an empty List if not found
+     * @return a List containing the InformationDTO objects if found, or an empty List if not found
      */
-    public List<Information> findByOrderByDateSendDesc() {
-        return dao.findByOrderByDateSendDesc();
+    public List<InformationDTO> findByOrderByDateSendDesc() {
+        List<Information> informations = dao.findByOrderByDateSendDesc();
+        return informations.stream()
+                .map(information -> new InformationDTO(
+                        information.getId(),
+                        information.getName(),
+                        information.getSurname(),
+                        information.getPhone(),
+                        information.getEmail(),
+                        dateConverterService.formatDateToFrontend(information.getDateSend()),
+                        information.getText(),
+                        information.getStatus()
+                ))
+                .collect(Collectors.toList());
     }
 
 }
