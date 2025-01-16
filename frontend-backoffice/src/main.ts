b64900bb406b2f7d195e1b21580c6065aa83cffa
BurbuliaAlessio02 <alessio.burbulia@edu.itspiemonte.it>
2025-01-16 13:43:05 +0100

    style: stylishing the login page for cascina caccia dashboard

--- frontend-backoffice/src/main.ts
+++ frontend-backoffice/src/main.ts
@@ -7,6 +7,4 @@ import "./global/styles/global.css";
 // local styles of the first page login admin
 import "./styles/style.css";
 
-
-
 //* -----------------------SCRIPTS IMPORTS-------------------------------------
