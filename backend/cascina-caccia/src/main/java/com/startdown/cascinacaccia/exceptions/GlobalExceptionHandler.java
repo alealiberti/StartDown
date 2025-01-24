a1c558dbf7a477cf07161f3f65f3c495dfdc21ca
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2025-01-25 00:11:11 +0100

    fix: removed usless import

--- backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/exceptions/GlobalExceptionHandler.java
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/exceptions/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import java.sql.SQLSyntaxErrorException;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
