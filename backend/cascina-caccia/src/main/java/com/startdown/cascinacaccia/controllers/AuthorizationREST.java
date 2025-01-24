1ea8c5930f35897ff6509b97d924affab8390981
alealiberti <alessandro.aliberti@edu.itspiemonte.it>
2025-01-24 22:37:59 +0100

    docs: added swagger docs in AuthoriaztionREST

--- backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/controllers/AuthorizationREST.java
+++ backend/cascina-caccia/src/main/java/com/startdown/cascinacaccia/controllers/AuthorizationREST.java
@@ -1,6 +1,11 @@
 package com.startdown.cascinacaccia.controllers;
 
 import com.startdown.cascinacaccia.services.JWTService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.extras.springsecurity6.auth.Authorization;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
@@ -31,6 +37,16 @@ public class AuthorizationREST {
      * @param authHeader the header containing the Basic auth
      * @return ResponseEntity containing the auth token or throws an exception if something went wrong
      */
+    @Operation(summary = "Login for the user", description = "Permits the login for the user and returns the authorization token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login done succesfully",
+                    content = @Content(mediaType = "application/json",
+                    schema = @Schema(example = "{\n" +
+                            "    \"token\": \"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdGFydGRvd25vd25lckBnbWFpbC5jb20iLCJpYXQiOjE3Mzc3NTM4ODYsImV4cCI6MTczNzc3MTg4Nn0.zvLqg1WOiqz9Z21T09jRzAOI_t4AGxo3hOWYPX87uLg\"\n" +
+                            "}"))),
+            @ApiResponse(responseCode = "403", description = "Wrong credentials",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestHeader("Authorization") String authHeader) {
         if (authHeader == null || !authHeader.startsWith("Basic ")) {
