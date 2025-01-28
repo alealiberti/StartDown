349e8f07c9241792a87090f253b2029ac36d69cd
Gabriele Speciale <gabspec@gmail.com>
2025-01-28 12:34:09 +0100

    feat: added final features on backoffice

--- frontend-backoffice/src/pages/settings/scripts/create-header.ts
+++ frontend-backoffice/src/pages/settings/scripts/create-header.ts
@@ -2,18 +2,20 @@
  * @file        main.ts
  * @author      Gabriele Speciale
  * @date        2025-01-26
- * @description 
+ * @description contains the function to dynamically create a header and prepend it to a specific section in the DOM
  */
 
 
 
-
+/**
+ * creates an h3 header element and prepends it to a section with the id 'requests'
+ * @param {string} HeaderText - the text content to insert into the header
+ */
 export function createHeader(HeaderText: string) {
-
-    // take the section element list which will be PREPENDED the header generate there
-    const section = document.querySelector("section#requests") as HTMLElement;
-
+    // create an h3 element and set its text content to the value of HeaderText
     const headerArchived = document.createElement("h3");
     headerArchived.textContent = `${HeaderText}`;
+
+    // select the section with id 'requests' and prepend the newly created header to it
     document.querySelector("section#requests")?.prepend(headerArchived);
 }
